e20d42e6d2eaf9ecd2506d965a7b92d2c333076e
sonyx <for.everything2287@gmail.com>
2026-06-11 10:41:05 +0300

    Create people.js

---
+++ people.js
@@ -0,0 +1,35 @@
+const people = [
+
+{
+id:"sofia",
+
+name:"София Орновскис",
+
+birth:"18.11.2009",
+
+country:"Эстония",
+
+mother:"ekaterina",
+father:"mihhailas",
+
+x:500,
+y:500
+},
+
+{
+id:"ekaterina",
+
+name:"Екатерина Орновскис",
+
+birth:"16.10.1983",
+
+country:"Эстония",
+
+mother:"lidiya",
+father:"nikolay",
+
+x:500,
+y:250
+}
+
+];
